8bccc0f950515d2f267317ad9c4df818361f65ae
Daniela-Ladino <diniela.ladino@gmail.com>
2023-11-07 21:54:11 -0500

    Pagina principal creada

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "../style/menu.css";
+
 export default function Menu() {
     return (
-        <p>soy menu</p>
+        <div className="menu">
+            <nav className="menu-nav">
+                <ul className="menu-ul">
+                    <li className="menu-li"><a href="#">Catálogo de productos</a></li>
+                    <li className="menu-li"><a href="#">Subastas</a></li>
+                    <li className="menu-li"><a href="#">Mis subastas</a></li>
+                    <li className="menu-li"><a href="#">Perfil</a></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
