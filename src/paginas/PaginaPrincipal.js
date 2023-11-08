49d8fc55d42648f871accf1acae6695fd4ad0785
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2023-11-07 21:03:51 -0500

    Base del front Luka

---
+++ src/paginas/PaginaPrincipal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Banner from "../componentes/Banner";
+import Menu from "../componentes/Menu";
+
+export default function PaginaPrincipal() {
+  return (
+    <React.Fragment>
+        <Banner/>
+        <Menu/>
+
+    </React.Fragment>
+  );
+}
