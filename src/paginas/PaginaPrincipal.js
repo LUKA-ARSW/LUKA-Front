f8c0e4e8fb962762d0597f8be3048580cdb32d50
Daniela-Ladino <diniela.ladino@gmail.com>
2023-11-07 23:13:43 -0500

    Pantalla base de la seccion subastas del menu

--- src/paginas/PaginaPrincipal.js
+++ src/paginas/PaginaPrincipal.js
@@ -3,11 +3,14 @@ import React from "react";
 import Banner from "../componentes/Banner";
 import Menu from "../componentes/Menu";
 
+import Subastas from "./Subastas"
+
 export default function PaginaPrincipal() {
   return (
     <React.Fragment>
         <Banner/>
         <Menu/>
+        <Subastas/>
 
     </React.Fragment>
   );
