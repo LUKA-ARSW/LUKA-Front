c6bfbde76ab0e18bd3428f1ab356f77aeb5e58a1
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-11-26 20:21:05 -0500

    Conexion de servicio de subastas cortas

--- src/componentes/MejoresPujas.js
+++ src/componentes/MejoresPujas.js
@@ -1,20 +1,51 @@
 import React from "react";
 
-export default function MejoresPujas({informacion}){
-    return(
-        <div className="mejoresPujas">
-            <ol>
-                {
-                    informacion.map((comprador, index)=>(
-                        <li key={index}>
-                            <p>Comprador: {comprador.nombre}</p>
-                            <p>Puja: {comprador.puja}</p>
-                        </li>
-                    ))
-                }
-            </ol>
+import salaServicios from "../servicios/shared/servicioSala.js";
 
-        </div>
+export default function MejoresPujas({sala, informacion, producto}){
+
+    const [compradoresActuales, setCompradoresActuales] = React.useState(informacion);
+
+    const realizarPuja = ()=>{
+        const puja = document.getElementById("pujar").value;
+        const comprador = {
+            nombre: "Daniela Ladino",
+            correo: "daniela.ladino@gmail.com"
+        };
+        salaServicios.pujarProducto(sala,comprador.correo,producto.idProducto,puja)
+            .then((respuesta)=>console.log(respuesta))
+            .then(()=>{
+                const compradoresTemp = [...compradoresActuales];
+                compradoresTemp.unshift(
+                    {
+                        first: comprador,
+                        second: puja
+                    }
+                );
+                setCompradoresActuales(compradoresTemp);
+            });
+    };
+
+    return(
+        <React.Fragment>
+            <div>
+                <input type="number" name="pujar" id="pujar"></input>
+                <button type="button" onClick={realizarPuja}>Pujar</button>
+            </div>
+            <div className="mejoresPujas">
+                <ol>
+                    {
+                        compradoresActuales.map((comprador, index)=>(
+                            <li key={index}>
+                                <p>Comprador: {comprador.first.nombre}</p>
+                                <p>Puja: {comprador.second}</p>
+                            </li>
+                        ))
+                    }
+                </ol>
+
+            </div>
+        </React.Fragment>
     );
 
 }
