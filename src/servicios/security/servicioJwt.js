6cd6140e88d478c9f3a3ca3babcf905e458b7248
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-12-01 22:30:27 -0500

    cambios jwt

--- src/servicios/security/servicioJwt.js
+++ src/servicios/security/servicioJwt.js
@@ -1,7 +1,7 @@
-import jwt from 'jsonwebtoken';
+
 
 function decryptToken(token) {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  //return jwt.verify(token, process.env.JWT_SECRET);
 }
 
 export default {
