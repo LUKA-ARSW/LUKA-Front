da731bff93119e42e8e9ac813d04bb8b08a2805c
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-12-04 16:36:31 -0500

    Vista catalogo productos conectada con servicio

--- src/componentes/CatalogoPaginadorElemento.jsx
+++ src/componentes/CatalogoPaginadorElemento.jsx
@@ -4,11 +4,11 @@ export default function CatalogoPaginadorElemento({elemento,accionConsulta}) {
     
     return(
         <React.Fragment>
-            <p>Id: {elemento.id}</p>
+            <p>Id: {elemento.idProducto}</p>
             <p>nombre: {elemento.nombre}</p>
             <p>Precio: {elemento.precio}</p>
             <image></image>
-             <button type="button" onClick={()=>accionConsulta(elemento.id)} >Consultar</button>
+             <button type="button" onClick={()=>accionConsulta(elemento.idProducto)} >Consultar</button>
         </React.Fragment>      
 
     );
