d0f3ed0c392e00fb5ce000a33d6a3544f84c25d7
20042000 <karol.ladino@mail.escuelaing.edu.co>
2023-12-04 01:06:08 -0500

    Redireccion de login implementada y modificacion pagina principal

--- src/paginas/PaginaPrincipal.jsx
+++ src/paginas/PaginaPrincipal.jsx
@@ -3,15 +3,12 @@ import React from "react";
 import Banner from "../componentes/Banner";
 import Menu from "../componentes/Menu";
 
-import Subastas from "../componentes/SubastasActivas"
+import Subastas from "../paginas/Subastas"
 
 export default function PaginaPrincipal() {
   return (
     <React.Fragment>
-        <Banner/>
-        <Menu/>
         <Subastas/>
-
     </React.Fragment>
   );
 }
