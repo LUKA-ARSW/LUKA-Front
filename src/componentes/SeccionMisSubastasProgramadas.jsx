a7a92fe398c3478f2466e7a47087d0346f684aec
MysticUnios1998 <eduarias08@gmail.com>
2023-12-30 20:11:30 -0500

    Carrusel generico implementado en las secciones correspondientes

--- src/componentes/SeccionMisSubastasProgramadas.jsx
+++ src/componentes/SeccionMisSubastasProgramadas.jsx
@@ -1,17 +1,45 @@
+import { PropTypes } from "prop-types";
+
 import React from "react";
+import { Link } from "react-router-dom";
 
-import CarruselMisSubastasProgramadas from "./CarruselMisSubastasProgramadas";
+import Carrusel from "@componentes/contenedor/Carrusel";
+import { ReactComponent as DeleteIcon } from '@assets/icon/trash-can.svg';
 
 
-export default function SeccionMisSubastasProgramadas(props) {
-    const titulo = props.titulo;
-    return(
-    <React.Fragment>
-        <h1>{titulo}</h1>
-        <CarruselMisSubastasProgramadas items={props.elementos}/>
+function SeccionMisSubastasProgramadas({ titulo, elementos }) {
 
-    </React.Fragment>
+    return (
+        <React.Fragment>
+            <h1>{titulo}</h1>
+            <Carrusel>
+                {
+                    elementos.map((item, index) => (
+                        <div key={index}>
+                            <p>Nombre: {item.nombre}</p>
+                            <p>Inicial el: {item.fechaInicio}</p>
+                            <p>Termina el: {item.fechaFin}</p>
+
+                            <button type="button">Modificar</button>
+                            <Link to={`/subastas/${item.nombre}/info`}>
+                                <button type="button">Consultar</button>
+                            </Link>
+                            <button type="button">
+                                <DeleteIcon />
+                            </button>
+                        </div>
+                    ))
+                }
+            </Carrusel>
+        </React.Fragment>
 
     );
 
-}          
+}
+
+SeccionMisSubastasProgramadas.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    elementos: PropTypes.array.isRequired
+};
+
+export default SeccionMisSubastasProgramadas;
--- src/componentes/SeccionSubasta.jsx
+++ src/componentes/SeccionSubasta.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+/* eslint-disable no-extra-semi */
+import PropTypes from "prop-types";
 
-import Carrusel from "./Carrusel";
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Carrusel from "@componentes/contenedor/Carrusel";
 import servicioJwt from "../servicios/security/servicioJwt";
 import servicioLocalStorage from "../servicios/web/servicioLocalStorage";
 
-export default function SeccionSubasta(props) {
-    const titulo = props.titulo; 
-
-    const inscribirUsuario = (subasta) => {
+function SeccionSubasta({ titulo, inscribirUsuario, elementos }) {
+    const inscribirUsuarioASubasta = (subasta) => {
         const usuario = servicioJwt.decryptToken(servicioLocalStorage.getValue("token"));
-        props.inscribirUsuario(usuario.correo, subasta);    
+        inscribirUsuario(usuario.correo, subasta);    
     };
     return (
         <React.Fragment>
             <h1>{titulo}</h1>
-            <Carrusel items={props.elementos} inscribirUsuario={inscribirUsuario}/>
+            <Carrusel>
+                {
+                    elementos.map((item, index) => (
+                        <div key={index}>
+                            <p>Nombre: {item.nombre}</p>
+                            <p>Inicial el: {item.fechaInicio}</p>
+                            <p>Termina el: {item.fechaFin}</p>
+                            <Link to={`/subastas/${item.nombre}/info`}>
+                                <button type="button">Consultar</button>
+                            </Link>
+                            <button type="button" onClick={() => inscribirUsuarioASubasta(item)}>Inscribirme</button>
+                        </div>
+                    ))
+                }
+            </Carrusel>
         </React.Fragment>
     );
-}
+};
+
+SeccionSubasta.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    elementos: PropTypes.array.isRequired,
+    inscribirUsuario: PropTypes.func.isRequired
+};
+
+
+export default SeccionSubasta;
